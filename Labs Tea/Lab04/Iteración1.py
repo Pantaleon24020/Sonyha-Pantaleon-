0c420830cb9a6475d203c9bdd3f50a26d84153ee
Pantaleon24020 <112120170+Pantaleon24020@users.noreply.github.com>
2022-09-28 21:37:33 -0600

    Add files via upload

---
+++ Labs Tea/Lab04/Iteración1.py
@@ -0,0 +1,14 @@
+contador=0
+valor=0
+while True:
+    numero=input("introduzca el numero: ")
+    if numero=="fin":
+        break
+    try:
+        valor=valor+int(numero)
+        contador=contador+1
+    except:
+        print("valor incorrecto")
+print("la suma de los numeros es: ", valor)
+print("el conteo de los numeros es: ", contador)
+print("el promedio de los numeros es: ", valor/contador)
